e703b39aa61d593e9d5c039464448222918c83f1
Daria Leskina <dleskinaleskina@yandex.ru>
2024-12-28 23:54:08 +0300

    feat: solve getIndicesOfOddNumbers task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -415,8 +415,15 @@ function getIdentityMatrix(/* n */) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const oddIdxArr = [];
+  numbers.map((item, index) => {
+    if (item % 2 !== 0) {
+      oddIdxArr.push(index);
+    }
+    return true;
+  });
+  return oddIdxArr;
 }
 
 /**
